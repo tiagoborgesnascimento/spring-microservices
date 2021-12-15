6a45a2cfbf5eed7cebbbe5aa10ccd05be3276343
Tiago Borges Nascimento <tiagoborgesnascimento2020@gmail.com>
2021-12-15 17:03:11 -0300

    api gateway, circuit break with resilience4j

---
+++ api-gateway/src/main/java/com/borgestech/apigateway/filter/LoggingFilter.java
@@ -0,0 +1,24 @@
+package com.borgestech.apigateway.filter;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+
+import reactor.core.publisher.Mono;
+
+@Component
+public class LoggingFilter implements GlobalFilter {
+
+	private Logger LOGGER = LoggerFactory.getLogger(getClass());
+	
+	@Override
+	public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+		
+		LOGGER.info("Path of the request received -> {} ", exchange.getRequest().getPath());
+		return chain.filter(exchange);
+	}
+
+}
---
+++ currency-exchange-service/src/main/java/com/borgestech/currencyexchangeservice/controller/CircuitBreakerController.java
@@ -0,0 +1,29 @@
+package com.borgestech.currencyexchangeservice.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import io.github.resilience4j.retry.annotation.Retry;
+
+@RestController
+public class CircuitBreakerController {
+
+	private Logger logger = LoggerFactory.getLogger(getClass());
+	
+	@GetMapping("/sample-api")
+	@Retry(name = "sample-api", fallbackMethod = "defaultResponse")
+	public String teste() throws Exception {
+		
+		logger.info("received request in /sample-api");
+		ResponseEntity<String> teste =  new RestTemplate().getForEntity("http://localhost:8080/some-dummy-url", String.class);			
+		return teste.getBody();
+	}
+	
+	public String defaultResponse(Exception ex) {
+		return "default response " + ex.getMessage();
+	}
+}
